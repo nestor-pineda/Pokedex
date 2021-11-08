8622b922c5cae85127b5c8b4c6c0c1a6d9c49338
Nestor Pineda <nestorpinedaelvira@gmail.com>
2021-11-08 18:31:54 +0100

    Añadido Logo SVG animation y efecto cristal en las Cards

--- pokedex.js
+++ pokedex.js
@@ -1,9 +1,14 @@
 "use strict";
 const pokedex = document.getElementById("pokedex");
+const previous = document.getElementById("previous");
+const next = document.getElementById("next");
 
-const fetchPokemon = () => {
+let offset = 1;
+let limit = 11;
+
+const fetchPokemon = (offset, limit) => {
   const promises = [];
-  for (let i = 1; i <= 150; i++) {
+  for (let i = offset; i <= offset + limit; i++) {
     const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
     promises.push(fetch(url).then((res) => res.json()));
   }
@@ -33,7 +38,19 @@ const displayPokemon = (pokemon) => {
   pokedex.innerHTML = pokemonHTMLString;
 };
 
-fetchPokemon();
+fetchPokemon(offset, limit);
+
+previous.addEventListener("click", () => {
+  if (offset != 1) {
+    offset -= 12;
+    fetchPokemon(offset, limit);
+  }
+});
+
+next.addEventListener("click", () => {
+  offset += 12;
+  fetchPokemon(offset, limit);
+});
 
 // la idea es crear un array con cada nueva promesas que nos da el for loop de los resultados que la api devuelve. Para esto usamos "push".
 // Luego usamos Promies.all en donde metemos el array con todas las promesas almacenadas.
@@ -42,3 +59,7 @@ fetchPokemon();
 // Para separar cada objeto con su información en el formato que deseamos lo hacemos con el map.
 
 // ahora creamos una funcion que imprima en el DOM los datos de los resultado de las promesas. Como map devuelve arrays, hacemos un join para concertirlo en string.
+
+// En la funcion fetchPokemon, usamos los parametros offset y limit para acotar el número de pokemos que queremos enseñar por página. Offset delimita desde que número de pokemon empieza el segmento y limit, hasta que pokemon llega el segmento.
+
+// Añado un event listener a cada boton previous y next para ir modificando los segmentos con el offset y limit. Previous bajará en 9 elementos el offset.Next subira en 9 elementos el offset.
